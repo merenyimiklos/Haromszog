ddd2a3c0979ccadeb5527e98a0a933995bdaf5bb
Unknown <myxy555@gmail.com>
2017-09-20 22:37:22 +0200

    első feltöltés
    első feltöltés

---
+++ src/haromszog/Haromszog.java
@@ -0,0 +1,35 @@
+package haromszog;
+
+import java.util.Scanner;
+
+public class Haromszog {
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        
+        int a,b,c = 0;
+        
+        System.out.println("Második feladat!");
+        
+        System.out.println("Kérem az 'a' oldal hosszát:");
+        a = sc.nextInt();
+        
+        System.out.println("Kérem a 'b' oldal hosszát:");
+        b = sc.nextInt();
+        
+        System.out.println("Kérem a 'c' oldal hosszát:");
+        c = sc.nextInt();
+        
+        if ((a+b) > c && (a+c) > b && (b+c) > a)
+        {
+            System.out.println("Ezek a számok 'háromszög számok'!");
+        }
+        else
+        {
+            System.out.println("Ezek a számok nem 'háromszög számok'!");
+        }
+		//created by Myxy
+    }
+    
+}
